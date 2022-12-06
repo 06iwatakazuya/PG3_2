f65cf78f2602984245d5555273b20a18814f8d2a
iwatakazuya <k021g1122@g.neec.ac.jp>
2022-12-06 14:25:51 +0900

    プロジェクト ファイルを追加します。

---
+++ PG3_Project2/main.cpp
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <list>
+#include <iterator>
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	list<const char*>Station = { "Osaki", "Gotanda", "Meguro", "Ebisu", "Shibuya", "Harajuku", "Yoyogi", "Shinjuku",
+								"Shin-Okubo", "Takadanobaba", "Mejiro", "Ikebukuro", "Otsuka", "Sugamo", "Komagome",
+								"Tabata", "Nippori", "Uguisudani", "Ueno", "Okachimachi", "Akihabara", "Kanda",
+								"Tokyo", "Yurakucho", "Shimbashi", "Hamamatsucho", "Tamachi", "Shinagawa" };
+
+	cout << "1970”N\n";
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		printf("%s\n", *itr);
+	}
+
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		if (*itr == "Nippori")
+		{
+			itr = Station.insert(itr, "Nishinippori");
+			itr++;
+		}
+	}
+
+	cout << "\n2019”N\n";
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		printf("%s\n", *itr);
+	}
+
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		if (*itr == "Shinagawa")
+		{
+			itr = Station.insert(itr, "Takanawa Gateway");
+			itr++;
+		}
+	}
+
+	cout << "\n2022”N\n";
+	for (auto itr = Station.begin(); itr != Station.end(); ++itr)
+	{
+		printf("%s\n", *itr);
+	}
+	return 0;
+}
